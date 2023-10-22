23078626ba2d4a6e7cae4c01767fc8af4e901d10
wq237wq@gmail.com <wq237wq@gmail.com>
2023-10-22 20:26:22 +0800

    fix

--- src/main/java/com/example/jfrog/demo/DemoApplication.java
+++ src/main/java/com/example/jfrog/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSON;
 
 
 @RestController
@@ -31,23 +31,9 @@ public class DemoApplication {
 //        JSON jsonObject = JSON.parseObject(payload);
 //		logger.info(jsonObject.toString());
 //
-//		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 
-		for (int i = 0; i < 1; i++) {
-			System.out.println("  *  ");
-			System.out.println(" *** ");
-			System.out.println("*****");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-			System.out.println("  *  ");
-		}
+
 	}
 
 }
